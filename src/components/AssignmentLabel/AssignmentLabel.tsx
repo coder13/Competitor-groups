45f45035910f71790f625b13d68a8002bbbe5c10
Choover <choover11@gmail.com>
2022-10-20 06:18:21 -0700

    Updated competitor page with new colors

--- src/components/AssignmentLabel/AssignmentLabel.tsx
+++ src/components/AssignmentLabel/AssignmentLabel.tsx
@@ -13,16 +13,22 @@ interface AssignmentLabelProps {
 }
 
 export default function AssignmentLabel({ assignmentCode }: AssignmentLabelProps) {
+  if (assignmentCode.match(/judge/i)) {
+    return <Container className="bg-blue-200">Judge</Container>;
+  }
+
   switch (assignmentCode) {
     case 'competitor':
       return <Container className="bg-green-200">Competitor</Container>;
     case 'staff-scrambler':
       return <Container className="bg-yellow-200">Scrambler</Container>;
-    case 'staff-judge':
-      return <Container className="bg-blue-200">Judge</Container>;
     case 'staff-runner':
       return <Container className="bg-orange-200">Runner</Container>;
+    case 'staff-dataentry':
+      return <Container className="bg-cyan-200">Data Entry</Container>;
+    case 'staff-announcer':
+      return <Container className="bg-violet-200">Announcer</Container>;
     default:
-      return <Container>{assignmentCode}</Container>;
+      return <Container>{assignmentCode.replace('staff-', '')}</Container>;
   }
 }
