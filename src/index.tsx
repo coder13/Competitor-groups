1bb4cfad88d139d13ce7e4d6800a0a69d2da7b15
Cailyn <choover11@gmail.com>
2023-07-27 17:15:02 -0400

    test 2

--- src/index.tsx
+++ src/index.tsx
@@ -18,3 +18,31 @@ root.render(<App />);
 reportWebVitals(undefined);
 
 // serviceWorkerRegistration.register();
+
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+self.addEventListener('fetch', (event) => {
+  // Check if this is a navigation request
+  if (event?.request?.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(
+      caches.open(cacheName).then((cache) => {
+        // Go to the network first
+        return fetch(event.request.url)
+          .then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
+
+            return fetchedResponse;
+          })
+          .catch(() => {
+            // If the network is unavailable, get from cache
+            console.log('loading from cache', event);
+            return cache.match(event.request.url);
+          });
+      })
+    );
+  } else {
+    return;
+  }
+});
