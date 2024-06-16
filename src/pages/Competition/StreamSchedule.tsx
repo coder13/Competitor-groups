1ef7e7751908134d07bac57e3618b4e61e65169c
Choover <choover11@gmail.com>
2024-06-16 07:43:39 -0700

    Fixed stream page

--- src/pages/Competition/StreamSchedule.tsx
+++ src/pages/Competition/StreamSchedule.tsx
@@ -1,14 +1,9 @@
 import { useCallback, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { useWCIF } from './WCIFProvider';
-import {
-  parseActivityCode,
-  streamActivities,
-  streamPersonIds,
-} from '../../lib/activities';
+import { parseActivityCode, streamActivities, streamPersonIds } from '../../lib/activities';
 import { formatDate, formatToParts, roundTime } from '../../lib/utils';
 import DisclaimerText from '../../components/DisclaimerText';
-import { Container } from '../../components/Container';
 
 export const byDate = (
   a: { startTime: string } | undefined,
@@ -68,7 +63,7 @@ export default function CompetitionStreamSchedule() {
   }
 
   const renderActivities = () => (
-    <Container>
+    <>
       <h4 className="text-xl mb-2 text-center">Live Stream</h4>
       <div className="shadow-md">
         <table className="w-full text-xs md:text-sm">
@@ -86,27 +81,24 @@ export default function CompetitionStreamSchedule() {
             {scheduleDays.map(({ date, dateParts }) => (
               <Fragment key={date}>
                 <tr>
-                  <td
-                    colSpan={6}
-                    className="font-bold text-lg text-center py-2">
+                  <td colSpan={6} className="font-bold text-lg text-center py-2">
                     {dateParts.find((i) => i.type === 'weekday')?.value || date}
                   </td>
                 </tr>
                 {getActivitiesByDate(date)
                   .sort(byDate)
                   .map((activity) => {
-                    const { eventId, roundNumber, groupNumber } =
-                      parseActivityCode(activity.activityCode);
+                    const { eventId, roundNumber, groupNumber } = parseActivityCode(
+                      activity.activityCode
+                    );
 
                     const venue = wcif.schedule.venues?.find((v) =>
                       v.rooms.some((r) => r.id === activity?.parent?.room?.id)
                     );
                     const timeZone = venue?.timezone;
 
-                    const roomName =
-                      activity?.room?.name || activity?.parent?.room?.name;
-                    const roomColor =
-                      activity?.room?.color || activity?.parent?.room?.color;
+                    const roomName = activity?.room?.name || activity?.parent?.room?.name;
+                    const roomColor = activity?.room?.color || activity?.parent?.room?.color;
                     const startTime = roundTime(
                       new Date(activity?.startTime || 0),
                       5
@@ -123,21 +115,15 @@ export default function CompetitionStreamSchedule() {
                         to={`/competitions/${wcif.id}/activities/${activity.id}`}>
                         <td className="py-2 text-center">{startTime}</td>
                         <td className="py-2 text-center">
-                          <span
-                            className={`cubing-icon event-${eventId} mx-1 text-xl`}
-                          />
+                          <span className={`cubing-icon event-${eventId} mx-1 text-xl`} />
                         </td>
                         <td className="py-2 text-center">{roundNumber}</td>
-                        <td className="py-2 text-center">
-                          {groupNumber || '*'}
-                        </td>
+                        <td className="py-2 text-center">{groupNumber || '*'}</td>
                         <td className="py-2 text-center">
                           <span
                             className="px-[6px]  py-[4px]  rounded-md"
                             style={{
-                              backgroundColor: roomColor
-                                ? `${roomColor}70`
-                                : 'inherit',
+                              backgroundColor: roomColor ? `${roomColor}70` : 'inherit',
                             }}>
                             {roomName}
                           </span>
@@ -158,7 +144,7 @@ export default function CompetitionStreamSchedule() {
           </tbody>
         </table>
       </div>
-    </Container>
+    </>
   );
 
   return (
@@ -166,11 +152,7 @@ export default function CompetitionStreamSchedule() {
       <DisclaimerText />
       <hr className="my-2" />
 
-      {activities.length > 0 ? (
-        renderActivities()
-      ) : (
-        <div>No Live Stream information</div>
-      )}
+      {activities.length > 0 ? renderActivities() : <div>No Live Stream information</div>}
     </div>
   );
 }
