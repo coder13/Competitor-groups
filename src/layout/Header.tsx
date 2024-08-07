499a8ff82cab463f8a0b8db615cba3fca196cb16
Cailyn Hoover <choover11@gmail.com>
2024-08-06 20:03:20 -0700

    moved logout menu to a user menu

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from '../providers/AuthProvider';
+import { Popover } from 'react-tiny-popover';
+import { useState } from 'react';
 
 export default function Header() {
   const { user, signIn, signOut } = useAuth();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(true);
+
   return (
     <header className="flex w-full shadow-md p-2 h-12 items-center print:hidden z-20 bg-white">
       <Link to="/" className="text-blue-500">
@@ -10,16 +14,29 @@ export default function Header() {
       </Link>
       <div className="flex flex-1" />
       {user ? (
-        <>
-          {user.avatar?.thumb_url && (
-            <div className="m-1 mr-2 w-8 h-8 relative flex justify-center items-center rounded-full bg-gray-500 text-xl text-white">
-              <img src={user.avatar?.thumb_url} alt={user.name} className="rounded-full" />
+        <Popover
+          isOpen={isPopoverOpen}
+          positions={['bottom']} // if you'd like, you can limit the positions
+          align="end"
+          onClickOutside={() => setIsPopoverOpen(false)} // handle click events outside of the popover/target here!
+          containerStyle={{
+            zIndex: '50',
+          }} // a style object to be applied to the popover container
+          content={
+            <div
+              className="bg-white border-2 shadow-xl mt-2 z-50"
+              onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+              <button onClick={signOut} className="text-blue-500 px-3 py-2 w-32 hover:bg-gray-100">
+                Logout
+              </button>
             </div>
-          )}
-          <button onClick={signOut} className="text-blue-500 mx-2">
-            Logout
-          </button>
-        </>
+          }>
+          <div
+            className="w-10 h-10 relative flex justify-center items-center rounded-full cursor-pointer"
+            onClick={() => setIsPopoverOpen((prev) => !prev)}>
+            <img src={user.avatar?.thumb_url || ''} alt={user.name} className="rounded-full" />
+          </div>
+        </Popover>
       ) : (
         <button onClick={signIn} className="text-blue-500 mx-2">
           Login
