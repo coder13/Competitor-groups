c570f1ee21c8cb2abcfbc9efdd369598340be6bb
Cailyn Hoover <choover11@gmail.com>
2025-05-13 21:51:42 -0700

    Started chinese translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en/translation.yaml';
+import es from './es/translation.yaml';
 import fr from './fr/translation.yaml';
+import ja from './ja/translation.yaml';
+import ko from './ko/translation.yaml';
+import zhCN from './zh-CN/translation.yaml';
 
 const resources = {
   en: { translation: en },
   fr: { translation: fr },
+  es: { translation: es },
+  ja: { translation: ja },
+  ko: { translation: ko },
+  'zh-CN': { translation: zhCN },
 };
 
 i18n.use(initReactI18next).init({
